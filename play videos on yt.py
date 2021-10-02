a99f89b8ed9ead89b195c082d3aa125955ed1f41
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2021-10-02 12:46:18 +0530

    Update play videos on yt.py

--- play videos on yt.py
+++ play videos on yt.py
@@ -2,7 +2,7 @@
 import pywhatkit
 url=input("Enter the title of the youtube video")
 print("playing your requested video:")
-pywhatkit.playonyt(url)#give the name of the video in this section
+pywhatkit.playonyt(url)#give the name of the video in this section or the url of the video
 #Be careful when naming the video as it always plays
 #the first video available on the search
 print("Bye! enjoy")
